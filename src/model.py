ac86aa537952906df1e2bf1d569c240c999e9864
Anurag Thapa <anurag.th.magar@gmail.com>
2026-04-18 01:50:56 -0500

    Add baseline and confusion matrix for BTC direcion model

--- src/model.py
+++ src/model.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 import pandas as pd
 import sklearn
-
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1] #resolve gives full absolute path
 DATA_PATH = PROJECT_ROOT / "data" / "btcusd.csv"
@@ -41,38 +40,47 @@ y = model_df["next_day_up"]
 #train&test split
 
 split_index = int(len(model_df) * 0.8)
-
 X_train = x.iloc[:split_index]
 X_test = x.iloc[split_index:]
-
 y_train = y.iloc[:split_index]
 y_test = y.iloc[split_index:]
-
 train_dates = model_df["Date"].iloc[:split_index]
 test_dates = model_df["Date"].iloc[split_index:]
 
 print("\n Train shape:", X_train.shape)
 print("Test shape:", X_test.shape)
-
 print("\nTrain data range:")
 print(train_dates.iloc[0], "to", train_dates.iloc[-1])
-
 print("\nTest data range:")
 print(test_dates.iloc[0], "to", test_dates.iloc[-1])
 
 model = LogisticRegression(max_iter=1000) #Build model
 model.fit(X_train,y_train) #train 
 y_pred = model.predict(X_test) #predict on test set
-
 accuracy = accuracy_score(y_test, y_pred) #actual answers vs predicted ans
 print(f"\n Logistic Regression Accuracy: {accuracy:.4f}")
 
 results_df = pd.DataFrame({
     "Date": test_dates.values,
     "Actual": y_test.values,
-    "Predicted": y_pred
-
-})
-
+    "Predicted": y_pred})
 print("\n First 10 predictions vs actual:")
 print(results_df.head(10))
+print("\n Actual value counts in test set:")
+print(y_test.value_counts())
+print("\n Predicted value countds:")
+print(pd.Series(y_pred).value_counts())
+#above for what real test set distribution VS what the model predicted
+
+baseline_up_accuracy =  (y_test == 1).mean()
+baseline_down_accuracy = (y_test == 0).mean()
+print("\n Basline accuracy if always predicting 1/up:")
+print(f"{baseline_up_accuracy:.4f}")
+print("\nBaseline accuracy of always predicting 0/down:")
+print(f"{baseline_down_accuracy:.4f}")
+
+cm = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:")
+print("[[TN, FP],") # 0(down)0,1
+print(" [FN, TP]]") #1(up)0,1
+print(cm)
